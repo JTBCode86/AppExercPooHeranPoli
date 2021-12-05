839b14ab5d562381cdbfff63d3d1bf383ac2c370
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-12-05 16:49:02 -0300

    C# - Adicionando ajustes ao exercicio proposto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using AppExercPooHeranPoli.Entities;
 
 namespace AppExercPooHeranPoli
 {
@@ -6,7 +9,48 @@ namespace AppExercPooHeranPoli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number of employees: ");
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hour: ");
+                int hours = int.Parse(Console.ReadLine());
+                
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalcharge = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    list.Add(new OutsourceEmployee(name, hours, valuePerHour, additionalcharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            
+            Console.WriteLine();
+
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name +" - $ "+ emp.Payment().ToString("F2",CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadLine();
         }
     }
 }
